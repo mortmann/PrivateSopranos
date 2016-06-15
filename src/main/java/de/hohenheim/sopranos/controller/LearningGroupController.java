77c02ee1ddae47d9edcdf9e4b42d6709db8a4e3c
mortmann <homerfreiheit@yahoo.de>
2016-06-15 15:09:14 +0200

    LG Options

--- src/main/java/de/hohenheim/sopranos/controller/LearningGroupController.java
+++ src/main/java/de/hohenheim/sopranos/controller/LearningGroupController.java
@@ -105,11 +105,22 @@ public class LearningGroupController {
         return "redirect:/learninggroup/home";
     }
 
-    @RequestMapping(value = "/learninggroup/home{name}")
-    public String lgHome(@RequestParam("name") String name, Model model, RedirectAttributes attr) {
-
-    	
-        return "/learninggroup/home";
+    @RequestMapping(value = "/learninggroup/option{name}", method = RequestMethod.GET)
+    public String lgOptionPre(@RequestParam("name") String name, Model model, RedirectAttributes attr) {
+    	LearningGroup lg = learningGroupRepository.findByName(name);
+    	model.addAttribute("group", lg);
+    	model.addAttribute("name",name);
+        attr.addAttribute("name", name);    	
+        return "/learninggroup/option";
+    }
+    @RequestMapping(value = "/learninggroup/option{name}", method = RequestMethod.POST)
+    public String lgOptionPost(@RequestParam("name") String name, LearningGroup group,Model model, RedirectAttributes attr) {
+    	LearningGroup lg = learningGroupRepository.findByName(name);
+    	lg.setDescription(group.getDescription());
+    	lg.setPassword(group.getPassword());
+        learningGroupRepository.save(lg);
+        attr.addAttribute("edited", "successful");
+        return "redirect:/learninggroup/home?name="+name;
     }
     @RequestMapping(value = "/learninggroup/users{name}", method = RequestMethod.GET)
     public String lgUser(@RequestParam("name") String name, Model model, RedirectAttributes attr) {
