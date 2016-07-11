c7bfc8a2cacd70042ce125221fad334d46a8d44a
mreichle <marvinreichle@aol.com>
2016-07-11 17:03:13 +0200

    alles mögliche und btn-lg fixed

--- src/main/java/de/hohenheim/sopranos/controller/PostController.java
+++ src/main/java/de/hohenheim/sopranos/controller/PostController.java
@@ -1,33 +1,23 @@
 package de.hohenheim.sopranos.controller;
 
 import de.hohenheim.sopranos.model.*;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * Created by Burakhan on 14.06.2016.
@@ -57,7 +47,6 @@ public class PostController {
     		post = postRepository.findOne(Integer.parseInt((String) request.getSession().getAttribute("postid")));
     		System.out.println(post);
     	}
-    	post = postRepository.getOne(post.getId());
     	if(post==null){
     		post = new Post();
     		post.setHeading("");
