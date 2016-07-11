3c96263e6847cd459bf2eec945c12c8b9baf5de1
brkmrk <ilikburi@live.de>
2016-07-11 20:41:02 +0200

    Stuff

--- src/main/java/de/hohenheim/sopranos/security/config/SecurityConfiguration.java
+++ src/main/java/de/hohenheim/sopranos/security/config/SecurityConfiguration.java
@@ -56,8 +56,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        // nur beim neuaufsetzen:auth.jdbcAuthentication().withDefaultSchema().dataSource(dataSource).passwordEncoder(passwordEncoder());
-        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
+        //auth.jdbcAuthentication().withDefaultSchema().dataSource(dataSource).passwordEncoder(passwordEncoder());
+       auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
     }
 
     /**
